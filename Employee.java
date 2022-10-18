89b3be79b74d64566cef31a4856ef98a6959a3f5
Tayfun DOGANAY <73904292+tayfundoganay@users.noreply.github.com>
2022-10-18 18:06:52 +0300

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,10 +1,10 @@
 public class Employee {
-    private String name;
-    private int salary;
-    private int workHours;
-    private int hireYear;
+    String name;
+    double salary;
+    int workHours;
+    int hireYear;
 
-    public Employee(String name, int salary, int workHours, int hireYear) {
+    public Employee(String name, double salary, int workHours, int hireYear) {
         this.name = name;
         this.salary = salary;
         this.workHours = workHours;
@@ -20,18 +20,18 @@ public class Employee {
 
     public double raiseSalary() {
         if (2021 - this.hireYear > 19) {
-            return (this.salary + bonus() - tax()) * 1.15;
+            return (this.salary * 0.15);
         } else if (2021 - this.hireYear > 9 && 2021 - this.hireYear < 20) {
-            return (this.salary + bonus() - tax()) * 1.10;
+            return (this.salary * 0.10);
         } else if (2021 - this.hireYear < 10) {
-            return (this.salary + bonus() - tax()) * 1.05;
+            return (this.salary * 0.05);
         }
         return 0;
     }
 
     @Override
     public String toString() {
-        return String.format("Name= %s\tSalary= %d\tworkHours= %d\thireYear= %d",
+        return String.format("Adi= %s\nMaasi= %.1f\nCalisma Saati= %d\nBaslangic Yili= %d",
                 this.name,this.salary,this.workHours,this.hireYear);
     }
 }
